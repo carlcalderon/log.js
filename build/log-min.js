3aa55684e502883cda0e89641304556f67d6e6f1
carlcalderon <carl.calderon@gmail.com>
2011-11-26 12:33:00 +0100

    Updated License - reduced minified size.

--- build/log-min.js
+++ build/log-min.js
@@ -1,15 +1,1 @@
-/*
-   Copyright 2011 Carl Calderon
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/function log(a){if(logging.verbose&&console&&console.log)if(a===undefined)console.log(logging.title+' - log message was "undefined".');else{a=String(a);if(a.match!==undefined)if(a.match(/^[a-z]{1}\s/)){var b="",c=0,d="log";switch(a.substr(0,1)){case"0":case"x":b="SYSTEM";c=0;break;case"1":case"c":b="CRITICAL";c=1;d="error";break;case"2":case"e":b="ERROR";c=2;d="error";break;case"3":case"w":b="WARNING";c=3;d="warn";break;case"4":case"i":b="INFO";c=4;d="info";break;default:case"d":b="DEBUG";c=5;d="debug"}c<=logging.level&&console[d](logging.title+"#"+b+" - "+a.substr(2))}else 6<=logging.level&&console.log(logging.title+" - "+a)}}function dump(a){logging.verbose&&console.log(a)}var logging={title:"log.js",verbose:!0,level:6};
+/* log.js, r2 - Copyright 2011 Carl Calderon - Licensed under the Apache License, Version 2.0 */function log(a){if(logging.verbose&&console&&console.log)if(a===undefined)console.log(logging.title+' - log message was "undefined".');else{a=String(a);if(a.match!==undefined)if(a.match(/^[a-z]{1}\s/)){var b="",c=0,d="log";switch(a.substr(0,1)){case"0":case"x":b="SYSTEM";c=0;break;case"1":case"c":b="CRITICAL";c=1;d="error";break;case"2":case"e":b="ERROR";c=2;d="error";break;case"3":case"w":b="WARNING";c=3;d="warn";break;case"4":case"i":b="INFO";c=4;d="info";break;default:case"d":b="DEBUG";c=5;d="debug"}c<=logging.level&&console[d](logging.title+"#"+b+" - "+a.substr(2))}else 6<=logging.level&&console.log(logging.title+" - "+a)}}function dump(a){logging.verbose&&console.log(a)}var logging={title:"log.js",verbose:!0,level:6};
