d22338a43ede5b7336f2527f9deeda3f7b68b6f2
Carl Calderon <carl.calderon@gmail.com>
2012-01-20 18:24:51 +0100

    initial jQuery version.
    Prepared plugin for plugins.jquery.com with package.json and minified
    branch.

---
+++ src/jquery.log.js
@@ -0,0 +1,99 @@
+/*! log.js, r4 - Copyright 2011 Carl Calderon - Licensed under the Apache License, Version 2.0 */
+(function( $ ){
+	
+	var logging = 
+	// OPTIONS:
+	{
+		title     : 'log.js',	// log title
+		verbose   : true,     	// logging (on/off)
+		align	  : true,		// align all message
+		_ff_align : navigator.userAgent.toLowerCase().indexOf('firefox') > -1,	// firefox ruin alignment
+		level     : 6
+		/*
+			0 system
+			1 critical
+			2 errors
+			3 warnings
+			4 info
+			5 debug
+			6 all
+		*/
+	};
+	
+	/**
+	 * Outputs the provided object and all public 
+	 * keys and/or variables.
+	 * 
+	 * @param obj Any object
+	 */
+	$.fn.dump = function( obj )
+	{
+		if( logging.verbose )
+			console.log( obj )
+	}
+	
+	/**
+	* Output a standardized message to the browser console.
+	* Can be switched of by setting #verbose to false.
+	* Level of detail is defined by #logLevel. Higher the
+	* value, the more information.
+	* Method is equal to console#log().
+	* @param msg   Any form of message.
+	*				Optional prefixes:
+	*				x   System
+	*				c   Critical
+	*				e   Error
+	*				w   Warning
+	*				i   Informational
+	*				d   Debug
+	*  			n/a Undefined type
+	*/
+	$.fn.log = function( msg ) {
+		if( logging.verbose )
+		if( console ) if( console.log ) 
+		{
+			if( msg === undefined )
+				console.log(logging.title + ' - log message was "undefined".');
+			else
+			{
+				if( typeof msg == 'object' )
+				{
+					if(msg.settings === undefined )
+					{
+						$.fn.dump(msg);
+					}
+					else
+					{
+						for( key in msg )
+							logging[key] = msg[key];
+					}
+				}
+				msg 		= String(msg);
+				var align	= logging.align?(logging._ff_align?'\t\t  ':'         '):'',
+					prefix  = '',
+					level   = 0,
+					func    = 'log';
+				if( msg.match !== undefined )
+				if( msg.match( /^[a-z]{1}\s/ ) )
+				{
+					switch( msg.substr(0,1) )
+					{	
+						case '0' : case 'x': prefix = 'SYSTEM';    align=!!logging._ff_align?'\t  ':'  ';   level = 0; break;
+						case '1' : case 'c': prefix = 'CRITICAL';  align=!!logging._ff_align?'  ':'';     level = 1; func = 'error'; break;
+						case '2' : case 'e': prefix = 'ERROR';     align=!!logging._ff_align?'\t  ':'   ';  level = 2; func = 'error'; break;
+						case '3' : case 'w': prefix = 'WARNING';   align=!!logging._ff_align?'\t':' ';    level = 3; func = 'warn';  break;
+						case '4' : case 'i': prefix = 'INFO';      align=!!logging._ff_align?'\t':'    '; level = 4; func = 'info';  break;
+						default :  case 'd': prefix = 'DEBUG';     align=!!logging._ff_align?'\t  ':'   ';  level = 5; func = 'debug'; break;
+					}
+					if( level <= logging.level )
+					{
+						if( !logging.align )
+							align = '';
+						console[func]( logging.title + '#'+prefix + align + ' | ' + msg.substr(2) );
+					}
+				}
+				else if( 6 <= logging.level ) console.log( logging.title + align + ' | ' + msg );
+			}
+		}
+	};
+})( jQuery );
--- utils/build.py
+++ utils/build.py
@@ -1,4 +1,7 @@
 import subprocess
 import os
 print os.getcwd()
+# free
 print subprocess.call('java -jar yuicompressor-2.4.7.jar -v -o '+os.getcwd()+'/../build/log-min.js '+os.getcwd()+'/../src/log.js', shell=True);
+# jQuery
+print subprocess.call('java -jar yuicompressor-2.4.7.jar -v -o '+os.getcwd()+'/../build/jquery.log-min.js '+os.getcwd()+'/../src/jquery.log.js', shell=True);
